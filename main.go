9969fdccfd44a95c12017c4818f7c49793ead47b
sunshineplan <sunshineplan@users.noreply.github.com>
2020-06-16 15:10:14 +0800

    Use custom command usage

--- main.go
+++ main.go
@@ -2,14 +2,31 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"os"
 	"strconv"
 	"strings"
 
 	"github.com/vharitonsky/iniflags"
 )
 
+func usage() {
+	fmt.Fprintf(flag.CommandLine.Output(), "Usage of %s:\n", os.Args[0])
+	fmt.Println(
+		`  update
+        Update Current Fire Emblem Heroes Voting Gauntlet Event Scoreboard to Database.
+
+  backup
+        Backup Fire Emblem Heroes Voting Gauntlet Scoreboard Database.
+
+  upload num int(default 0)
+        Upload Fire Emblem Heroes Voting Gauntlet Event num's Results to Github Repository.
+        Value 0 will treat as current event`)
+}
+
 func main() {
+	flag.Usage = usage
 	flag.StringVar(&MetadataConfig.Server, "server", "", "Metadata Server Address")
 	flag.StringVar(&MetadataConfig.VerifyHeader, "header", "", "Verify Header Header Name")
 	flag.StringVar(&MetadataConfig.VerifyValue, "value", "", "Verify Header Value")
