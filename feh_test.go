8ad9925c9ea5c869c940edabf0d8565af8387ed9
sunshineplan <sunshineplan@users.noreply.github.com>
2024-04-27 10:37:40 +0800

    Update feh_test.go

--- feh_test.go
+++ feh_test.go
@@ -8,6 +8,10 @@ import (
 func TestFEH(t *testing.T) {
 	_, _, fullScoreboard, err := Scrape()
 	if err != nil {
+		if err == ErrEventNotOpen {
+			log.Print(err)
+			return
+		}
 		t.Fatal(err)
 	}
 	for _, i := range fullScoreboard {
